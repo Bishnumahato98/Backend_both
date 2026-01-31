8a0c4da93d24cc4a0314992cc62b10fb56e10154
Bishnu Mahato <bishnukumarmahato262@gmail.com>
2026-01-31 20:33:24 +0545

    first

---
+++ src/types/user.type.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+export const UserSchema = z.object({
+    email: z.email().min(5),
+    password: z.string().min(8),
+    username: z.string().min(3).max(30),
+    firstName: z.string().optional(),
+    lastName: z.string().optional(),
+    role: z.enum(['user', 'admin']).default('user'),
+    imageUrl: z.string().optional() // this line
+});
+
+export type UserType = z.infer<typeof UserSchema>;
